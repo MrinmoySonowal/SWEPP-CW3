2d0810af82c79f750251519d70a2d3e6a1ceb751
MrinmoySonowal <M.Sonowal@sms.ed.ac.uk>
2021-03-27 21:08:38 +0400

    Added implementation for register supermarket
    Signed-off-by: MrinmoySonowal <M.Sonowal@sms.ed.ac.uk>

--- uoe-sepp2021-coursework3-skeleton/src/main/java/shield/SupermarketClientImp.java
+++ uoe-sepp2021-coursework3-skeleton/src/main/java/shield/SupermarketClientImp.java
@@ -5,14 +5,34 @@
 package shield;
 
 public class SupermarketClientImp implements SupermarketClient {
+  /** The string representation of the base server endpoint (a HTTP address) */
+  private String endpoint;
   private String name;
   private int phoneNum;
+  private final String REG_NEW = "registered new";
+  private final String ALR_REG = "already registered";
   public SupermarketClientImp(String endpoint) {
+    this.endpoint = endpoint;
   }
 
   @Override
   public boolean registerSupermarket(String name, String postCode) {
-    return false;
+    // construct the endpoint request
+    String request = String.format("/registerSupermarket?business name=%s&postcode=%s", name, postCode);
+    try {
+      String response = ClientIO.doGETRequest ( endpoint + request);
+      boolean isValidResponse = (response.equals(REG_NEW) || response.equals(ALR_REG));
+      if(!isValidResponse) {
+        String errMsg = String.format("WARNING: Unexpected response for %s", request);
+        System.err.println(errMsg);
+        return false;
+      }
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      return false;
+    }
+    return true;
   }
 
   // **UPDATE**
