02ece0466817a215f6b127103ab8920e2a90988c
MrinmoySonowal <M.Sonowal@sms.ed.ac.uk>
2021-03-28 18:28:59 +0400

    Implemented the methods in CateringCompanyClientImp
    Signed-off-by: MrinmoySonowal <M.Sonowal@sms.ed.ac.uk>

--- uoe-sepp2021-coursework3-skeleton/src/main/java/shield/CateringCompanyClientImp.java
+++ uoe-sepp2021-coursework3-skeleton/src/main/java/shield/CateringCompanyClientImp.java
@@ -5,11 +5,35 @@
 package shield;
 
 public class CateringCompanyClientImp implements CateringCompanyClient {
+  private final String REG_NEW = "registered new" ;
+  private final String ALR_REG = "already registered" ;
+  private String endpoint;
+  private String name;
+  private String postCode;
+  private boolean isRegistered;
+
   public CateringCompanyClientImp(String endpoint) {
+    this.endpoint = endpoint;
   }
 
   @Override
   public boolean registerCateringCompany(String name, String postCode) {
+    String request = String.format("/registerCateringCompany?business_name=%s&postcode=%s",name,postCode);
+    try {
+      String response = ClientIO.doGETRequest(this.endpoint + request);
+      boolean isValidResponse = response.equals(REG_NEW)||response.equals(ALR_REG);
+      if(!isValidResponse){
+        String errMsg = "";
+        System.err.println(errMsg);
+        return false;
+      }
+    } catch(Exception e) {
+      e.printStackTrace();
+      return false;
+    }
+    this.name = name;
+    this.postCode = postCode;
+    this.isRegistered = true;
     return false;
   }
 
@@ -20,16 +44,16 @@ public class CateringCompanyClientImp implements CateringCompanyClient {
 
   @Override
   public boolean isRegistered() {
-    return false;
+    return this.isRegistered;
   }
 
   @Override
   public String getName() {
-    return null;
+    return this.name;
   }
 
   @Override
   public String getPostCode() {
-    return null;
+    return this.postCode;
   }
 }
