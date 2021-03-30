59b89dc271a22024657d9e9fea8ebfc935b397c9
Dale Huang <huangzihan98@gmail.com>
2021-03-31 01:34:49 +0800

    finishe FoodBoxOrder class implementation, added getters and setters
    Signed-off-by: Dale Huang <huangzihan98@gmail.com>

--- uoe-sepp2021-coursework3-skeleton/src/main/java/shield/FoodBoxOrder.java
+++ uoe-sepp2021-coursework3-skeleton/src/main/java/shield/FoodBoxOrder.java
@@ -8,16 +8,68 @@ public class FoodBoxOrder {
     public FoodBoxOrder(int foodBoxID){
     }
 
-    private DietType dietType;
-    private OrderStatus orderStatus;
+    private String dietType;
+    private String orderStatus;
     private DeliveryStatus deliveryStatus;
     private String deliveryService;
     private Dictionary<Integer, Integer> itemIDs;
-    private Dictionary<DietType, Collection<Integer>> dietDefaultItemIDs;
+    //private Dictionary<DietType, Collection<Integer>> dietDefaultItemIDs;
     private String orderSource;
     private LocalDateTime deliveryTime;
 
+    public String getDietType() {
+        return dietType;
+    }
 
+    public String getOrderStatus() {
+        return orderStatus;
+    }
 
+    public DeliveryStatus getDeliveryStatus() {
+        return deliveryStatus;
+    }
 
+    public String getDeliveryService() {
+        return deliveryService;
+    }
+
+    public Dictionary<Integer, Integer> getItemIDs() {
+        return itemIDs;
+    }
+
+    public String getOrderSource() {
+        return orderSource;
+    }
+
+    public LocalDateTime getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public void setDietType(String dietType) {
+        this.dietType = dietType;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public void setDeliveryStatus(DeliveryStatus deliveryStatus) {
+        this.deliveryStatus = deliveryStatus;
+    }
+
+    public void setDeliveryService(String deliveryService) {
+        this.deliveryService = deliveryService;
+    }
+
+    public void setItemIDs(Dictionary<Integer, Integer> itemIDs) {
+        this.itemIDs = itemIDs;
+    }
+
+    public void setOrderSource(String orderSource) {
+        this.orderSource = orderSource;
+    }
+
+    public void setDeliveryTime(LocalDateTime deliveryTime) {
+        this.deliveryTime = deliveryTime;
+    }
 }
